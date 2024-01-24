956594e0bd22b07c266a84928144d6cb4231e987
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-24 13:51:29 -0300

    feat: controller function register user

---
+++ src/controller/UserControler.js
@@ -0,0 +1,60 @@
+const UserModel = require("../model/UserModel")
+const brcypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const jwtSecret = "oljkçlndcfkonwlkfcnoihoiefihpei2he3flmnefçlnçlkwefnçlknpi"
+
+//Generate user token
+const generateToken = (id) => {
+  return jwt.sign({id}, jwtSecret, {
+    expiresIn: "7d"
+  })
+}
+
+const registerUser = async (req, res) => {
+
+  const { name, lastName, email, password } = req.body
+  const id = Math.floor(Math.random() * 10) + new Date().getMilliseconds()
+
+  try {
+
+    const user = await UserModel.findOne({ where: { email } })
+
+    //Check if user exist
+    if (user) {
+      res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] })
+      return
+    }
+
+    //Generate passwrod hash
+    const salt = await brcypt.genSaltSync()
+    const passwordHash = await brcypt.hashSync(password, salt)
+
+    const newUser = await UserModel.create({
+      id, name, lastName, email, password: passwordHash
+    })
+
+
+    if(!newUser) {
+      res.status(422).json({
+        errors: ["Houve um erro, por favor tente novamente mais tarde."]
+      })
+
+      return
+    }
+
+    res.status(201).json({
+      id: newUser.id,
+      token: generateToken(newUser.id)
+    })
+
+  }catch(error){
+    console.log(error)
+  }
+
+
+}
+
+
+module.exports = {
+  registerUser
+}
