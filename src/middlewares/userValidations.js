1f6a08cb2a81b59eab4cca027bc4fd8f121127dd
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-24 14:40:40 -0300

    feat: create middleware validation of login

--- src/middlewares/userValidations.js
+++ src/middlewares/userValidations.js
@@ -41,7 +41,21 @@ const userRegisterValidation = () => {
   ];
 };
 
+const userLoginValidation = () => {
+  return [
+    body("email")
+      .isString()
+      .withMessage("O e-mail é obrigatório.")
+      .isEmail()
+      .withMessage("Insira um e-mail válido"),
+    
+      body("password")
+    .isString()
+    .withMessage("A senha é obrigatória.")
+  ];
+};
 
 module.exports = {
-  userRegisterValidation
+  userRegisterValidation,
+  userLoginValidation
 }
