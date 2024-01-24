1d8c62335f37bc1a30baf8350273b86d142667bb
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-24 12:24:56 -0300

    feat: creation DB

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize")
+
+const connection = new Sequelize("connected_people", "root", "14109015", {
+  host: "localhost",
+  dialect: "mysql"
+})
+
+
+const connect = () => {
+
+  connection  
+    .authenticate()
+    .then(() => {})
+    .catch(error => console.log(error))
+
+}
+
+
+module.exports = {connect}
