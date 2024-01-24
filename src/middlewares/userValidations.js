39553136ce19118c89c55d054c0cfaa9ca3d7bbf
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-24 13:52:29 -0300

    feat: create middleware of validation request user

---
+++ src/middlewares/userValidations.js
@@ -0,0 +1,47 @@
+const { body } = require("express-validator");
+
+const userRegisterValidation = () => {
+  return [
+    body("name")
+      .isString()
+      .withMessage("O nome é obrigatório.")
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa ter no mínimo 3 caracteres."),
+
+    body("lastName")
+      .isString()
+      .withMessage("O sobrenome é obrigatório.")
+      .isLength({min: 3})
+      .withMessage("O sobrenome precisa ter no mínimo 3 caracteres."),
+
+    body("email")
+      .isString()
+      .withMessage("O e-mail é obrigatório")
+      .isEmail()
+      .withMessage("Insira uma e-mail válido email@email.com"),
+
+    body("password")
+      .isString()
+      .withMessage("A senha é obrigatória")
+      .isLength({min: 3})
+      .withMessage("A senha precisa ter 3 caracteres."),
+
+    body("confirmPassword")
+      .isString()
+      .withMessage("É necessário confirmar a sua senha.")
+      .custom((value, {req}) => {
+
+        if(value !== req.body.password){
+            throw new Error("A senhas não são iguais")
+        }
+
+        return true
+
+      })
+  ];
+};
+
+
+module.exports = {
+  userRegisterValidation
+}
