d479049707913376119401cd62baa6b55214d23e
Eric Ricielle <suporte@tucanoweb.com.br>
2024-02-26 18:35:44 -0300

    Update userValidations.js
    - Use the generic function to avoid unnecessary repetition;
    - Use an object to store the validations' array and return with the spread operator.

--- src/middlewares/userValidations.js
+++ src/middlewares/userValidations.js
@@ -1,61 +1,66 @@
 const { body } = require("express-validator");
 
+//Generic validation function
+const validateField = (field, min) => {
+  return body(field)
+    .isString()
+    .withMessage(errorMessages[field].required)
+    .isLength({ min })
+    .withMessage(errorMessages[field].minLength(min))
+}
+
+//Error messages object
+const errorMessages = {
+  name: {
+    required: "O nome é obrigatório.",
+    minLength: (min) => `O nome precisa ter no mínimo ${min} caracteres.`,
+  },
+  lastName: {
+    required: "O sobrenome é obrigatório.",
+    minLength: (min) => `O sobrenome precisa ter no mínimo ${min} caracteres.`,
+  },
+  email: {
+    required: "O e-mail é obrigatório.",
+    invalid: "Insira uma e-mail válido email@email.com",
+  },
+  password: {
+    required: "A senha é obrigatória.",
+    minLength: (min) => `A senha precisa ter ${min} caracteres.`,
+  },
+  confirmPassword: {
+    required: "É necessário confirmar a sua senha.",
+    notMatch: "A senhas não são iguais",
+  },
+}
+
 const userRegisterValidation = () => {
-  return [
-    body("name")
-      .isString()
-      .withMessage("O nome é obrigatório.")
-      .isLength({ min: 3 })
-      .withMessage("O nome precisa ter no mínimo 3 caracteres."),
+  const validations = [
+    validateField("name", 3),
+    validateField("lastName", 3),
+    validateField("email", 1).isEmail().withMessage(errorMessages.email.invalid),
+    validateField("password", 3),
+    validateField("confirmPassword", 1).custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error(errorMessages.confirmPassword.notMatch)
+      }
 
-    body("lastName")
-      .isString()
-      .withMessage("O sobrenome é obrigatório.")
-      .isLength({min: 3})
-      .withMessage("O sobrenome precisa ter no mínimo 3 caracteres."),
+      return true;
+    }),
+  ]
 
-    body("email")
-      .isString()
-      .withMessage("O e-mail é obrigatório")
-      .isEmail()
-      .withMessage("Insira uma e-mail válido email@email.com"),
-
-    body("password")
-      .isString()
-      .withMessage("A senha é obrigatória")
-      .isLength({min: 3})
-      .withMessage("A senha precisa ter 3 caracteres."),
-
-    body("confirmPassword")
-      .isString()
-      .withMessage("É necessário confirmar a sua senha.")
-      .custom((value, {req}) => {
-
-        if(value !== req.body.password){
-            throw new Error("A senhas não são iguais")
-        }
-
-        return true
-
-      })
-  ];
-};
+  return [...validations]
+}
 
 const userLoginValidation = () => {
-  return [
-    body("email")
-      .isString()
-      .withMessage("O e-mail é obrigatório.")
-      .isEmail()
-      .withMessage("Insira um e-mail válido"),
-    
-      body("password")
-    .isString()
-    .withMessage("A senha é obrigatória.")
-  ];
-};
+  const validations = [
+    validateField("email", 1).isEmail().withMessage(errorMessages.email.invalid),
+    validateField("password", 1),
+  ]
+
+  return [...validations]
+}
 
 module.exports = {
   userRegisterValidation,
-  userLoginValidation
+  userLoginValidation,
 }
